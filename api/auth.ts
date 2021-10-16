b4b05b73642385096e738e01c52019adeaa0956b
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2021-10-16 23:36:37 +0400

    initial authentication implementation

--- api/auth.ts
+++ api/auth.ts
@@ -1,7 +1,23 @@
 import { Router } from "../depts.ts";
-
+import { createJWT } from "./util/jwt.ts";
 const router = new Router();
 
-router.post("/auth", (ctx) => {});
+router.post("/auth", (ctx) => {
+  ctx.response.body = {
+    message: "APP running",
+    data: {},
+  };
+}).get("/auth/me", async (ctx) => {
+  const accessToken = await createJWT({
+    user: "admin",
+    id: "someid",
+  });
+  ctx.response.body = {
+    message: "Success",
+    data: {
+      access_token: accessToken,
+    },
+  };
+});
 
 export { router };
